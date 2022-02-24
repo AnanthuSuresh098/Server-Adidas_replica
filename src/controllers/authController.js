173e5dc4b5f611e42755a7253d519a3e292ac400
sivacreator <93380641+SivanandamG@users.noreply.github.com>
2022-02-24 13:50:15 +0530

    signup and login part

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -55,11 +55,59 @@ router.post(
   }
 );
 
+router.get(
+  "/signup",
+  async (req, res) => {
+    try {
+     const errors = validationResult(req);
+     // console.log(errors);
+     // if (!errors.isEmpty()) {
+     //   return res.status(400).json({ errors: errors.array() });
+     // }
+
+     var user = await Users.findOne({email:req.body.email}).lean().exec();
+     console.log(req.body);
+    if(user !== null){
+         return res.status(401).send("User exists login");
+     }
+
+      user = await Users.create(req.body);
+
+     const token = newToken(user);
+
+     return res.send({ user:user,token:token });
+
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+  }
+);
+
+router.get("/users", async (req, res) => {
+  try {
+    let user = await Users.find().lean().exec();
+    // if(user === null){
+    //      return res.status(401).send({res:"User doesn't exists"});
+    //  }
+    //  const match =  function(password) {
+    //       console.log(password,user.password);
+    //       return  bcrypt.compareSync(password, user.password);
+    //     };
+    //     if(!match(req.body.password)){
+    //          return res.status(401).send("Wrong password");
+    //     };
+    //     const token = newToken(user);
+     return res.status(201).send({user:user});
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
 
 const newToken = (user) => {
   return jwt.sign({ user: user }, "adidasscretkey", {
     expiresIn: 60 * 60 * 10,
   });
 };
+
 
 module.exports = router;
